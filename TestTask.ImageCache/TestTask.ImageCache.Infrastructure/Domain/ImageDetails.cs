5a08a3800c46a69024c02bd431148df0bccbe9f8
sheiler <silvio.heiler@gmail.com>
2021-02-21 20:21:16 -0300

    fix search error

--- TestTask.ImageCache/TestTask.ImageCache.Infrastructure/Domain/ImageDetails.cs
+++ TestTask.ImageCache/TestTask.ImageCache.Infrastructure/Domain/ImageDetails.cs
@@ -11,9 +11,12 @@ namespace TestTask.ImageCache.Infrastructure.Domain
         public string tags { get; set; }
         public string full_picture { get; set; }
 
-        public override string ToString()
+        public override string ToString() // ugly
         {
-            return string.Format("{0}{1}{2}",this.author.ToLower(), this.camera.ToLower(), this.tags.ToLower()); 
+            return string.Format("{0}{1}{2}",
+               this.author == null ? "" :  this.author.ToLower(),
+               this.camera == null ? "" : this.camera.ToLower(),
+               this.tags == null ? "" : this.tags.ToLower()) ; 
         }
 
     }
